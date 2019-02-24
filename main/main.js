c0fe21071261a422fab45cc57126d70f495af920
BlueSkylover <36990081+BlueSkylover@users.noreply.github.com>
2019-02-24 16:01:39 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -4,15 +4,20 @@
 //         2<s<=8    Freight_fee=6+(s-2)*0.8;
 //         8<s       Freight_fee=6+(8-2)*0.8+(s-8)*0.8*1.5;
 module.exports =function fizzbuzz(number,time){
-     let times=(time%60>0)?(parseInt(time/60)+1):(time/60);
-     let fee=6+times*0.25;
-     if(number>2 && number<=8){
-         fee += (number-2)*0.8;
+     if(number<0 || time<0){
+         return "Input value is abnormal.";
      }
-     if(number>8){
-         fee +=6*0.8+(number-8)*0.8*1.5;
+     else{
+         let times=(time%60>0)?(parseInt(time/60)+1):(time/60);
+         let fee=6+times*0.25;
+         if(number>2 && number<=8){
+             fee += (number-2)*0.8;
+         }
+         if(number>8){
+             fee +=6*0.8+(number-8)*0.8*1.5;
+         }
+         return Math.round(fee);
      }
-     return Math.round(fee);
 }
 
 
