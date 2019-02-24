b3ff6df72732c6c145db0e24c6bf8ed73eba3776
BlueSkylover <36990081+BlueSkylover@users.noreply.github.com>
2019-02-24 16:05:07 +0800

    Update main-spec.js

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -9,25 +9,25 @@ chai.use(sinonChai);
 var main = require("../main/main");
 describe('taxi fee', function () {
     // write your tests here...
-    it("returns the money  when distance less than 0 kilometres", function(){
+    it("returns the error when given the number is wrong", function(){
         let number=-5;
         let time=20;
         let result=main(number,time);
         expect(result).to.equal("Input value is abnormal.");
     });
-    it("returns the money  when distance less than 2 kilometres", function(){
+    it("returns the money when the distance less than 2 kilometres", function(){
         let number=1;
         let time=20;
         let result=main(number,time );
         expect(result).to.equal(6);
     });
-    it("returns the money  when distance more than 2 kilometres and less than 8 kilometres ", function(){
+    it("returns the money when the distance more than 2 kilometres and less than 8 kilometres ", function(){
         let number=5;
         let time=20;
         let result=main(number,time );
         expect(result).to.equal(9);
     });
-    it("returns the money  when distance more than 8 kilometres ", function(){
+    it("returns the money when the distance more than 8 kilometres ", function(){
         let number=12;
         let time=20;
         let result=main(number,time );
